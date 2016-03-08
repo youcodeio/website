98a53c1d0e10102a6568b1d33f88b75b25aeaaff
stevenw <watremez.steven@gmail.com>
2016-03-08 16:30:12 +0100

    add channel son api

---
+++ src/api/channels/channels-api.js
@@ -0,0 +1,178 @@
+[	
+	{
+		"channel_name":"Google I/O",
+		"description":"An annual software developer-focused conference held by Google in San Francisco, California",
+		"channel_banner":"",
+		"channel_mobileBanner":"",
+		"channel_link":""
+		"channel_videos" :[
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":""
+			},
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":"",
+				"video_publishedAt":""
+			}
+		]
+	},
+	{
+		"channel_name":"Google I/O",
+		"description":"An annual software developer-focused conference held by Google in San Francisco, California",
+		"channel_banner":"",
+		"channel_mobileBanner":"",
+		"channel_link":""
+		"channel_videos" :[
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":""
+			},
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":"",
+				"video_publishedAt":""
+			}
+		]
+	},
+	{
+		"channel_name":"Google I/O",
+		"description":"An annual software developer-focused conference held by Google in San Francisco, California",
+		"channel_banner":"",
+		"channel_mobileBanner":"",
+		"channel_link":""
+		"channel_videos" :[
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":""
+			},
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":"",
+				"video_publishedAt":""
+			}
+		]
+	},
+	{
+		"channel_name":"Google I/O",
+		"description":"An annual software developer-focused conference held by Google in San Francisco, California",
+		"channel_banner":"",
+		"channel_mobileBanner":"",
+		"channel_link":""
+		"channel_videos" :[
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":""
+			},
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":"",
+				"video_publishedAt":""
+			}
+		]
+	},
+	{
+		"channel_name":"Google I/O",
+		"description":"An annual software developer-focused conference held by Google in San Francisco, California",
+		"channel_banner":"",
+		"channel_mobileBanner":"",
+		"channel_link":""
+		"channel_videos" :[
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":""
+			},
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":"",
+				"video_publishedAt":""
+			}
+		]
+	},
+	{
+		"channel_name":"Google I/O",
+		"description":"An annual software developer-focused conference held by Google in San Francisco, California",
+		"channel_banner":"",
+		"channel_mobileBanner":"",
+		"channel_link":""
+		"channel_videos" :[
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":""
+			},
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":"",
+				"video_publishedAt":""
+			}
+		]
+	},
+	{
+		"channel_name":"Google I/O",
+		"description":"An annual software developer-focused conference held by Google in San Francisco, California",
+		"channel_banner":"",
+		"channel_mobileBanner":"",
+		"channel_link":""
+		"channel_videos" :[
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":""
+			},
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":"",
+				"video_publishedAt":""
+			}
+		]
+	},
+	{
+		"channel_name":"Google I/O",
+		"description":"An annual software developer-focused conference held by Google in San Francisco, California",
+		"channel_banner":"",
+		"channel_mobileBanner":"",
+		"channel_link":""
+		"channel_videos" :[
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":""
+			},
+			{
+				"video_name":"google test sa mère",
+				"video_description":"terrible cette vidéo !!!!!!!!",
+				"video_link":"https://www.google.com/events/io/io14videos",
+				"video_thumbnail":"",
+				"video_publishedAt":""
+			}
+		]
+	}
+]
